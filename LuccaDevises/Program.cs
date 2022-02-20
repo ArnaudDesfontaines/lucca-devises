3b368638f2f42a9829bc0f8ebbd057af24642597
Arnaud Desfontaines <desfontainesarnaud@gmail.com>
2022-02-20 13:15:34 +0100

    retrieve args

---
+++ LuccaDevises/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace LuccaDevises
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] firstArgs = args[0].Split(';');
+            Int32 devisesTabLength = Int32.Parse(args[1]);
+            string[] devisesTab = new string[devisesTabLength];
+            for (int i = 0; i < devisesTabLength; i++)
+            {
+                devisesTab[i] = args[i + 2];
+            }
+
+            Console.WriteLine("f" + firstArgs[0] + "l" + devisesTabLength + "p" + devisesTab[0]);
+        }
+    }
+}
